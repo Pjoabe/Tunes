47193e4d2f1c2249e382519fe234cdc8ef6de627
joabe <joabe9999pereira@gmail.com>
2022-10-07 02:36:52 -0300

    requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Header from '../Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../Loading';
 
 class Search extends Component {
   state = {
     name: '',
     disabled: true,
+    loading: false,
+    apiData: [],
+    foundResults: false,
   };
+
+  componentDidMount() {
+    this.searchByName();
+  }
 
   validateButton = () => {
     const { name } = this.state;
@@ -26,10 +36,62 @@ class Search extends Component {
     }), () => this.validateButton());
   };
 
+  searchByName = async () => {
+    const { name } = this.state;
+    const apiReq = await searchAlbumsAPI(name);
+    this.setState(
+      () => ({ apiData: apiReq }),
+      () => {
+        this.validateApiResult();
+      },
+    );
+  };
+
+  validateApiResult = () => {
+    const { apiData } = this.state;
+    const biggerThan = apiData.length > 0;
+    if (biggerThan) {
+      this.setState({ foundResults: true });
+    } else {
+      this.setState({ foundResults: false });
+    }
+  };
+
+  renderApiData = () => {
+    const { apiData, requiredData } = this.state;
+    const albums = (
+      <>
+        <h4>
+          Resultado de álbuns de:
+          {/* https://techstacker.com/how-to-add-white-space-between-elements-react-jsx/ */}
+          {' '}
+          {requiredData}
+        </h4>
+        {apiData.map(({ artworkUrl100, collectionName, collectionId, artistName }) => (
+          <div key={ collectionId }>
+            <img
+              src={ artworkUrl100 }
+              alt={ collectionName }
+            />
+            <span>{collectionName}</span>
+            <span>{artistName}</span>
+            <NavLink
+              to={ `/album/${collectionId}` }
+              data-testid={ `link-to-album-${collectionId}` }
+            >
+              Ir Para
+            </NavLink>
+          </div>))}
+      </>);
+    return albums;
+  };
+
   render() {
-    const { name, disabled } = this.state;
+    const { name, disabled, loading, foundResults } = this.state;
     return (
       <div data-testid="page-search">
+        {loading && <Loading /> && this.renderApiData()}
+        {foundResults || <span>Nenhum álbum foi encontrado</span>}
         <Header />
         <form>
           <label htmlFor="searchInput">
@@ -46,6 +108,14 @@ class Search extends Component {
             type="submit"
             disabled={ disabled }
             data-testid="search-artist-button"
+            onClick={ () => {
+              this.setState(() => ({
+                name: '',
+                requiredData: name,
+                loading: true,
+              }));
+              this.searchByName();
+            } }
           >
             Pesquisar
           </button>
