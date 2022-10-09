df90d45ffc8a592ec9ea21c6088aabe2e52a13a3
joabe <joabe9999pereira@gmail.com>
2022-10-09 16:09:09 -0300

    requisito 8

--- src/pages/musicCard.jsx
+++ src/pages/musicCard.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  state = {
+    favorite: false,
+    loading: false,
+  };
+
+  addToFavorite = async () => {
+    const { favorite } = this.state;
+    const { songs } = this.props;
+    if (favorite === true) {
+      this.setState({ loading: true });
+      await addSong(songs);
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
-    const { songs: { trackName, previewUrl } } = this.props;
+    const { songs: { trackName, previewUrl, trackId } } = this.props;
+    const { favorite, loading } = this.state;
     return (
       <div>
-        <span>{ trackName }</span>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-        </audio>
+        {(loading && <Loading />) || (
+          <>
+            <span>{trackName}</span>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+            </audio>
+            <form>
+              <label htmlFor={ trackId }>
+                Favorita
+                <input
+                  data-testid={ `checkbox-music-${trackId}` }
+                  type="checkbox"
+                  id={ trackId }
+                  checked={ favorite }
+                  onChange={ ({ target: { checked } }) => this.setState({
+                    favorite: checked,
+                  }, this.addToFavorite) }
+                />
+              </label>
+            </form>
+          </>
+        )}
       </div>
     );
   }
